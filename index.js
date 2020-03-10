3153707afa794e0f90e98c951322003125ee2aef
Blobby Blobfish <aywang6@gmail.com>
2020-03-10 23:21:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(players, instruments) {
 
 var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 
-function johnLennonFacts(facts ) {
+function johnLennonFacts(facts) {
     var newFacts = [];
     var i = 0;
     while (i < facts.length) {
